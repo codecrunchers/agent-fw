9f7e52b7ae262567d0b2c37c211e3be7ed598aef
Alan Ryan <alan@codecrunchers.ie>
2023-12-23 23:21:33 +0000

    working with file and convo

--- app/main.py
+++ app/main.py
@@ -1,23 +1,42 @@
 from fastapi import File, Request, UploadFile
-from langchain.chains import LLMChain
+from langchain.chains import ConversationalRetrievalChain, LLMChain
+from langchain.chains.loading import _load_qa_with_sources_chain
 from langchain.memory import vectorstore
-from app import langchain_llm, app, prompt, files, vectorstore, logger
+from app import langchain_llm, app, prompt, files, vectorstore, logger, memory
+import io
 
 
 @app.post("/upload")
 async def upload(file: bytes = File(...)):
-    import io
-
     f = io.BytesIO(file)
     await files.process_upload(f, vectorstore)
-    return {"summary": "File proecessed"}
+    return {"summary": "File processed"}
 
 
 @app.get("/analyse/{query}")
 async def analyse(request: Request, query):
     _prompt = prompt.generate()
-    conversation = LLMChain(
-        llm=langchain_llm, prompt=_prompt, verbose=True, memory=request.state.mem
+    vstore = vectorstore.load("key")
+
+    question_generator = LLMChain(llm=langchain_llm, prompt=_prompt)
+    from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+
+    doc_chain = load_qa_with_sources_chain(langchain_llm, chain_type="map_reduce")
+
+    chain = ConversationalRetrievalChain(
+        retriever=vstore.as_retriever(),
+        question_generator=question_generator,
+        combine_docs_chain=doc_chain,
     )
-    response = conversation({"question": query.strip()})
-    return {"summary": response["text"].strip()}
+
+    bot = ConversationalRetrievalChain.from_llm(
+        langchain_llm, vstore.as_retriever(), memory=request.state.mem, verbose=False
+    )
+    history = memory
+    response = bot(
+        {
+            "question": query.strip(),
+            "chat_history": history.load(request.state.session_id),
+        }
+    )
+    return {"summary": response["answer"]}

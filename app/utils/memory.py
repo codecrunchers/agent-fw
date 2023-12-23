c6f0bc27339d7c8d25ad6c2731371e7f67822607
Alan Ryan <alan@codecrunchers.ie>
2023-12-23 16:52:29 +0000

    Working LocalMemory

--- app/utils/memory.py
+++ app/utils/memory.py
@@ -7,21 +7,40 @@ from app import config
 def memory_factory():
     if config["memory"] == "in_mem":
         return InMemMemory()
+    if config["memory"] == "local":
+        return OnDiskLocalMemory()
     else:
         raise ValueError("Unsupported memory")
 
 
 class AbstractMemory(ABC):
     @abstractmethod
-    def save(self, session_id):
+    def save(self, session_id, user, ai):
         pass
 
     @abstractmethod
-    def load(self, sesss):
+    def load(self, session_id):
         pass
 
 
 class InMemMemory(AbstractMemory):
+    memory = None
+
+    def __init__(self):
+        self.memory = ConversationBufferMemory(
+            memory_key="chat_history", return_messages=True
+        )
+
+    def save(self, session_id, user, ai):
+        self.memory.chat_memory.add_user_message(user)
+        if ai:
+            self.memory.chat_memory.add_ai_message(ai)
+
+    def load(self, session_id):
+        return self.memory
+
+
+class OnDiskLocalMemory(AbstractMemory):
     def save(self, session_id, user, ai):
         self.memory = ConversationBufferMemory(memory_key="chat_history")
         self.memory.chat_memory.add_user_message(user)
